298fd5325ea749e7ef3672618f8fa414c883a13f
Arif Ibrahim <31169171+arif87@users.noreply.github.com>
2019-03-21 19:16:57 +0600

    Update validation.py
    Fixed the issue. The issue was, validation could not validate between two data types. After invoking validateType method's loop ran only for once and return true.

--- src/factory/validation.py
+++ src/factory/validation.py
@@ -3,24 +3,27 @@ from datetime import datetime
 
 class Validator(object):
     def validate_type(self, element, desired_type):
+        if desired_type == "int":
+            return type(element) == int
         if desired_type == "string":
             return type(element) == str
+            # return True
         if desired_type == "datetime":
             return isinstance(element, datetime)
         if desired_type == "float":
             return type(element) == float
-        if desired_type == "int":
-            return type(element) == int
         if type(desired_type) == list:
             return (element in desired_type)
-
         raise ValueError("Invalid value for desired type")
 
     def validateTypes(self, element, fields):
+       
         for field in fields:
             if field in element:
-                if self.validate_type(element[field], fields[field]):
-                    return True
+                if not self.validate_type(element[field], fields[field]):
+                    return False
+                    # raise ValueError("Invalid value for desired type")
+        return True
 
     def validate(self, element, fields, required_fields, optional_fields):
         if not self.validateTypes(element, fields):
@@ -29,6 +32,7 @@ class Validator(object):
         element_fields = set(element.keys())
         required_fields = set(required_fields)
         optional_fields = set(optional_fields)
+
 
         if len(required_fields - element_fields) > 0:
             raise ValueError("Required field missing")
